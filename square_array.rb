1d4b627f8a8c369666c45b38aaf6c9d69dad588c
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-12 02:51:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   # your code here
 new_array = []
-array.each do |integer|
-  new_array << integer ** 2
+array.each do |numbers|
+  new_array << numbers ** 2
 end
 return new_array
 end
